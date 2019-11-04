77aef080a87cfa9dc2a4a1a79626aee582db7199
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2019-11-04 16:51:05 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -7,35 +7,32 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
-function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+//context
+import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 
-	const addItem = item => {
+function App() {
+	const [ products ] = useState(data);
+	const [ cart, setCart ] = useState([]);
+
+	const addItem = (item) => {
 		// add the given item to the cart
+		setCart([ ...cart, item ]);
+		//[...cart] is saying take all the values in the array and put them out, so needs to be in an array.
 	};
 
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
+		<ProductContext.Provider value={{ products, addItem }}>
+			<CartContext.Provider value={{ cart }}>
+				<div className='App'>
+					<Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
-
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
+					{/* Routes */}
+					<Route exact path='/' component={Products} />
+					<Route path='/cart' render={() => <ShoppingCart cart={cart} />} />
+				</div>
+			</CartContext.Provider>
+		</ProductContext.Provider>
 	);
 }
 
